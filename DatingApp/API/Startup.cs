a3972ecaaf8f7c530b6731988dcb39aa71d6696c
Ilan Cherniavsky <Omnislasher1808@gmail.com>
2021-12-29 20:17:17 +0200

    end of section 2

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -33,6 +33,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +50,11 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("https://localhost:4200/"));
 
             app.UseAuthorization();
 
--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
   }
 
   getUsers(){
-    this.http.get("http://localhost:5000/api/users").subscribe({
+    this.http.get("https://localhost:5001/api/users").subscribe({
       next: (data) => {
         this.users = data;
       },
