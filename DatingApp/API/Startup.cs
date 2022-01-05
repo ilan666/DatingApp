1501a67b1c3f2529298c31dbe7288afb7ef8adad
Ilan Cherniavsky <Omnislasher1808@gmail.com>
2022-01-05 21:14:47 +0200

    Starting of section 4

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -71,7 +71,7 @@ namespace API
             app.UseCors(policy => policy
             .AllowAnyHeader()
             .AllowAnyMethod()
-            .WithOrigins("https://localhost:4200/"));
+            .WithOrigins("https://localhost:55335/"));
 
             app.UseAuthentication();
 
